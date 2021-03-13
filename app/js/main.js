4fd270bbe91095d74d31675ce5d0c5aa651b0d68
StacyIko <9huston@mail.ru>
2021-03-14 00:09:02 +0100

    added intro

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function() {
+	// $('.burger, .menu a').on('click', function () {
+	// 	$('.menu').toggleClass('menu--active');
+	// })
 
+	$('.intro').slick({
+		dots: true,
+		arrows: false
+	});
 });
